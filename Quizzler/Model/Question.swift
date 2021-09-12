372a09528f6b71dcc6514ecd6a8beda90d4c3971
Yunus Gedik <yunus-gedik@outlook.com>
2021-09-12 23:33:07 +0300

    feat: progress bar for quiz, 12 quiz questions and visual feedback

---
+++ Quizzler/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Yunus Gedik on 12.09.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question {
+    let question: String?
+    let answer: String?
+    
+    init(q: String, a: String){
+        self.question = q
+        self.answer = a
+    }
+}
---
+++ Quizzler/Model/QuizBrain.swift
@@ -0,0 +1,9 @@
+//
+//  QuizBrain.swift
+//  Quizzler
+//
+//  Created by Yunus Gedik on 12.09.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
